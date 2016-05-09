2a0bf132538dadc925acc26a68f0de9d3cd1f346
fingoldin <vassilioskaxiras@gmail.com>
2016-05-09 09:43:05 -0400

    Create core.h

---
+++ core.h
@@ -0,0 +1,54 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <string>
+#include <vector>
+#include <cmath>
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transformation.hpp>
+
+struct Vertex {
+    glm::vec3 Position;
+    
+    glm::vec3 Normal;
+    
+    glm::vec2 UV;
+    
+    GLuint Texture;
+};
+
+class Core
+{
+public:
+    
+    void begin(void);
+    
+    void end(void);
+    
+    void update(void);
+    
+    void render(void);
+    
+    void clearScreen(void);
+    
+    void updateScreen(void);
+    
+    Core() {}
+    ~Core() {}
+    
+    Vertex getVertexOfIndex(GLuint index) { return this->vertices[index]; }
+    
+    
+private:
+    
+    std::vector<Vertex> vertices;
+    
+};
+
+#endif
