b25926854c066179bee2af78c732c3e12be876d3
fingoldin <vassilioskaxiras@gmail.com>
2016-05-13 11:06:41 -0400

    Update mesh.h

--- mesh.h
+++ mesh.h
@@ -7,26 +7,25 @@
 #include <GL/glew.h>
 
 #include "shader.h"
+#include "loadtexture.h"
+
+struct Texture {
+    std::string path;
+    GLuint id;
+}; 
 
 class Mesh
 {
 public:
     
-    Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, int numtex, 
-         const char * tex1, 
-         const char * tex2 = NULL, 
-         const char * tex3 = NULL, 
-         const char * tex4 = NULL,
-         const char * tex5 = NULL, 
-         const char * tex6 = NULL, 
-         const char * tex7 = NULL, 
-         const char * tex8 = NULL
-    );
+    Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, int numtex, std::vector<std::string> texpaths);
     ~Mesh() {}
     
     void render(Shader * shader);
 
 private:
+    
+    GLuint VAO, VBO;
     
     GLfloat * position_data;
     GLfloat * normal_data;
@@ -34,27 +33,35 @@ private:
     GLuint * index_data;
     
     int num_texs;
+    
+    std::vector<Texture> textures;
+
 };
 
-Mesh::Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, int numtex,
-           const char * tex1, 
-           const char * tex2 = NULL, 
-           const char * tex3 = NULL, 
-           const char * tex4 = NULL,
-           const char * tex5 = NULL, 
-           const char * tex6 = NULL, 
-           const char * tex7 = NULL, 
-           const char * tex8 = NULL
-);
+Mesh::Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, std::vector<std::string> texpaths)
 {
     this->position_data = positions;
     this->normal_data = normals;
     this->UV_data = UVs;
     this->index_data = indices;
     
-    this->num_texs = numtex
+    this->num_texs = texpaths.size();
     
     for(int i = 0; i < this->num_texs; i++)
-        this->tex_paths[i] = 
+    {
+        Texture tex;
+        tex.path = texpaths[i];
+        tex.id = AlphaTextureFromFile(texpaths[i].c_str(), false);
+        
+        this->textures.push_back(tex);
+    }
+    
+    glGenVertexArrays(1, &this->VAO);
+    glGenBuffers(1, &this->VBO);
+    
+    glBindVertexArray(this->VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+}
 
 #endif
