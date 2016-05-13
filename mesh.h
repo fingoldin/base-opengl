680a10565fef10f7c768603b6f31ddf898477cdc
fingoldin <vassilioskaxiras@gmail.com>
2016-05-13 10:07:31 -0400

    Update mesh.h

--- mesh.h
+++ mesh.h
@@ -1,10 +1,22 @@
 #ifndef MESH_H
 #define MESH_H
 
+#include <GL/glew.h>
+
+#include "shader.h"
+
 class Mesh
 {
-
 public:
     
+    Mesh(GLfloat * vertices, GLuint * triangles);
+    ~Mesh() {}
+    
+    void render(Shader * shader)
+
+private:
+    
+    GLfloat * vertex_data;
+    GLfloat * triangle_data;
 
 #endif
