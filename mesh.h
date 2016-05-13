12c8c7cfdff6e1392d677a279b3c9771cfb05c29
fingoldin <vassilioskaxiras@gmail.com>
2016-05-13 10:38:23 -0400

    Update mesh.h

--- mesh.h
+++ mesh.h
@@ -12,14 +12,49 @@ class Mesh
 {
 public:
     
-    Mesh(GLfloat * vertices, GLuint * triangles);
+    Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, int numtex, 
+         const char * tex1, 
+         const char * tex2 = NULL, 
+         const char * tex3 = NULL, 
+         const char * tex4 = NULL,
+         const char * tex5 = NULL, 
+         const char * tex6 = NULL, 
+         const char * tex7 = NULL, 
+         const char * tex8 = NULL
+    );
     ~Mesh() {}
     
     void render(Shader * shader);
 
 private:
     
-    GLfloat * vertex_data;
-    GLfloat * triangle_data;
+    GLfloat * position_data;
+    GLfloat * normal_data;
+    GLfloat * UV_data;
+    GLuint * index_data;
+    
+    int num_texs;
+};
+
+Mesh::Mesh(GLfloat * positions, GLfloat * normals, GLfloat * UVs, GLuint * indices, int numtex,
+           const char * tex1, 
+           const char * tex2 = NULL, 
+           const char * tex3 = NULL, 
+           const char * tex4 = NULL,
+           const char * tex5 = NULL, 
+           const char * tex6 = NULL, 
+           const char * tex7 = NULL, 
+           const char * tex8 = NULL
+);
+{
+    this->position_data = positions;
+    this->normal_data = normals;
+    this->UV_data = UVs;
+    this->index_data = indices;
+    
+    this->num_texs = numtex
+    
+    for(int i = 0; i < this->num_texs; i++)
+        this->tex_paths[i] = 
 
 #endif
