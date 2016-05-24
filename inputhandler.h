e271aad2be350dcab2ed50a90285d79816b3278f
fingoldin <vassilioskaxiras@gmail.com>
2016-05-24 08:04:42 -0400

    Update inputhandler.h

--- inputhandler.h
+++ inputhandler.h
@@ -5,11 +5,21 @@ class InputHandler
 {
 public:
     
-    static void 
+    static void begin(GLFWwindow * window);
+    
+    static void end();
+    
+    static void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode);
+    static void scroll_callback(GLFWwindow * window, double xoffset, double yoffset);
+    static void mouse_callback(GLFWwindow * window, double xpos, double ypos);
+    
+    static void update(GLfloat dtime);
     
 private:
     
     InputHandler() { }
+    
+    static bool keys[1024];
     
 };
 
